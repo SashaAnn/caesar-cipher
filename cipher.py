1b517d4475ab12bc31ff072302856e3edfed2bf7
Sasha <sastephens888@outlook.com>
2024-11-17 20:14:32 -0500

    Complete

--- cipher.py
+++ cipher.py
@@ -1,1 +1,17 @@
-# add your code here
+abc = 'abcdefghijklmnopqrstuvwxyz'
+
+shift = 5
+plain_text = input('Please enter a sentence:')
+plain_text = plain_text.lower()
+
+print(f'The encrypted sentence is: ')
+secret_text = ''
+
+for char in plain_text:
+    if char in abc:
+        index = abc.find(char)
+        index = (index + shift) % 26
+        char = abc[index]
+    secret_text += char
+
+print(secret_text)
